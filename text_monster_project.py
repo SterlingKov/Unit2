fa94abd62dcb5d800f7fb2b37460d5d148582d40
Sterling <sterling.kovash@gmail.com>
2022-02-15 10:01:42 -0600

    now its working

--- text_monster_project.py
+++ text_monster_project.py
@@ -1,7 +1,7 @@
 
 
 dungeon = [
-    ['empty', 'monster', 'sword', 'stairs up', 'sword'],
+    ['sword', 'monster', 'sword', 'stairs up', 'empty'],
     ['sword', 'magic stone', 'stairs down', 'monster', 'stairs up'],
     ['magic stone', 'stairs down', 'empty', 'boss monster', 'prize']
 ]
@@ -11,7 +11,7 @@ dungeon = [
 current_floor = 0
 current_room = 0
 location = dungeon[current_floor][current_room]
-print(location)
+
 
 inventory = []
 
@@ -19,26 +19,30 @@ inventory = []
 
 while True:
     move_finished = True
-    
-    move_input = input("What would you like to do? -left- -right- -up- -down- -grab- -fight- -help-")
+    print(f"you are located in: {location}")
+    move_input = input("What would you like to do? -left- -right- -up- -down- -fight- -run- -grab- -help-")
 
-    if move_input == 'left':
+        
+    if move_input == 'left': 
         current_room -= 1
-        print(location)
+        location = dungeon[current_floor][current_room]
+        
         if current_room == -1:
             current_room += 1
             print("You can't move any further in that direction")
     
-    if move_input == 'right':
+    elif move_input == 'right':
         current_room += 1
-        print(location)
+        location = dungeon[current_floor][current_room]
+        
         if current_room == 5:
             current_room -= 1
             print("You can't move any further in that direction")
 
-    if move_input == 'up':
+    elif move_input == 'up':
         if location == 'stairs up':
             current_floor += 1
+            location = dungeon[current_floor][current_room]
             print(location)
             if current_floor == 3:
                 current_floor -= 1
@@ -46,9 +50,10 @@ while True:
         else: 
             print("You can't go up...")
 
-    if move_input == 'down':
+    elif move_input == 'down':
         if location == 'stairs down':
             current_floor -= 1
+            location = dungeon[current_floor][current_room]
             print(location)
             if current_floor == -1:
                 current_floor += 1
@@ -58,22 +63,37 @@ while True:
     
 
     if location == 'monster':
-        print(location)
-        while move_finished:
-            move_input = input("You've encountered a dangerous monster. What would you like to do? -fight- -run-")
-            if move_input == 'fight':
-                if inventory.index('sword') > -1:
-                    inventory.pop(inventory.index('sword'))
-                    print("You have slain the monster!")
-                    move_finished = False
-                else:
-                    print("You didn't have a sword, and thus you have died.")
-                    print("YOU LOST")
-                    break
-            elif move_input == 'run':
-                current_room -= 1
-                print("You were able to escape from the monster")
-                print(location)
+        location = dungeon[current_floor][current_room] 
+        move_input = input("You've encountered a dangerous monster. What would you like to do? -fight- -run-")
+        if move_input == 'fight':
+            if 'sword' in inventory:
+                inventory.pop(inventory.index('sword'))
+                print("You have slain the monster!")
+                location = 'empty'
                 move_finished = False
             else:
-                print("Enter a valid input")
+                print("You didn't have a sword, and thus you have died.")
+                print("YOU LOST")
+                break
+        elif move_input == 'run':
+            current_room -= 1
+            print("You were able to escape from the monster")
+            location = dungeon[current_floor][current_room]
+            print(location)
+            move_finished = False
+        else:
+            print("Enter a valid input")
+
+    elif move_input == 'grab':
+        if location == 'sword':
+            inventory.append('sword')
+            location = 'empty'
+        elif location == 'magic stones':
+            inventory.append('magic stones')
+        else:
+            print('There is nothing to grab')
+
+    else:
+        print("not valid")
+                
+        
